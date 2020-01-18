b1170b5b5ffb9aaeabc95612ad56f360df2744a8
abdelhakhamdouni <abdelhakhamdouni@sfr.fr>
2020-01-18 18:55:31 +0100

    un visge en css

---
+++ 404.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <img width=100% height=100% src="https://isarta.fr/infos/wp-content/uploads/2019/04/page404-airbnb-1024x494.png" alt="">
+</body>
+</html>
